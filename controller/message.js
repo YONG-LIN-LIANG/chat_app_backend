543881777d2a57088eb9b76732c0f7fb725af249
Steven <steven841221@gmail.com>
2022-06-29 12:53:56 +0800

    feat: pair feature

--- controller/message.js
+++ controller/message.js
@@ -9,26 +9,34 @@ exports.handleSendMessage = async(req, res, next) => {
   const redisDB = await redis()
   console.log('identity', identity)
   if(identity === 0) {
+    console.log('reqq body', req.body)
     const { memberId, roomId, questionId, question, questionContent, answer } = req.body
+    let cs_id
+    let formatQuestionContent
+    console.log('questionCon', questionContent)
+    formatQuestionContent = questionContent.toString()
+    if(questionId === 1) {
+      formatQuestionContent = questionContent.toString()
+    } else if(questionId === 2) {
+      formatQuestionContent = JSON.stringify(questionContent)
+      cs_id = questionContent.find(i => i.name === answer).id
+    }
+    console.log('questionContenttt', formatQuestionContent, answer)
     // 處理系統訊息發送訊息
     if(roomId === 0) {
       // 處理未配對的系統訊息(目前還未有這狀況)
       // 建立smessage-memberId-0的redis陣列，
-      const isRoomExist = await redisDB.get(`smessage-${memberId}-0`)
-      
-      if(isRoomExist === null) {
-        const currentTime = handleFormatDateTime(new Date().toISOString())
-        const message = `1;${questionId};${question};${questionContent};${currentTime};${answer}`
-        console.log('messageee', message)
-        const result = await redisDB.rPush(`smessage-${memberId}-0`, message)
-        if(result === 1){
-          // 塞值成功，返回成功
-          return res.status(201).send()
+      const currentTime = handleFormatDateTime(new Date().toISOString())
+      const message = `1;${questionId};${question};${formatQuestionContent};${currentTime};${answer}`
+      console.log('messageee', message)
+      const result = await redisDB.rPush(`smessage-${memberId}-0`, message)
+      if(result){
+        // 塞值成功，返回成功
+        res.status(201).send({currentTime, cs_id})
 
-        }
-        console.log('result', result)
       }
-      console.log('isRoomExist', isRoomExist)
+      console.log('result', result)
+      
     }else {
       // 處理已配對的系統訊息(目前還未有這狀況)
     }
@@ -38,4 +46,7 @@ exports.handleSendMessage = async(req, res, next) => {
   } else {
 
   }
+
+  await redisDB.disconnect()
+  return
 }
